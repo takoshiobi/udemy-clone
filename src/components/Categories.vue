0a8fd975991155b669e8f758c920165c87871d66
akinariobi <yuliyanasher@gmail.com>
2019-04-21 21:23:46 +0300

    add icons

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -4,44 +4,52 @@
         <div class="row">
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
+                    <img src="../assets/icons/coding.png" alt="" />
                     Development
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/analytics.png" alt="" />
+                    Business
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/monitor.png" alt="" />
+                    IT and Software
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/sketch.png" alt="" />
+                    Design
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/seo.png" alt="" />
+                    Marketing
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/open-book.png" alt="" />
+                    Personal Development
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/camera.png" alt="" />
+                    Photography
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 col-12 col-sm-12 col-xl-3">
                 <div class="topItem">
-                    Development
+                    <img src="../assets/icons/music.png" alt="" />
+                    Music
                 </div>
             </div>
         </div>
@@ -50,11 +58,10 @@
 
 <style scoped>
 .topItem {
-    text-align: center;
     border: 1px solid;
     padding: 20px 0;
     margin-top: 20px;
-    font-size: 18px;
+    font-size: 16px;
     border: 1px solid #e3e3e4;
     box-shadow: 0px 2px 4px #e3e3e4;
 }
@@ -70,5 +77,11 @@ h3 {
 
 .categories {
     padding-bottom: 40px;
+}
+
+.topItem img {
+    width: 30px;
+    margin-left: 20px;
+    margin-right: 10px;
 }
 </style>
